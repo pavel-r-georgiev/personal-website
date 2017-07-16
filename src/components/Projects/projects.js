00d8c67e0965833c0e3471e9481f3e41263115c5
Pavel Georgiev <pavel@brainnwave.com>
2017-07-16 14:51:44 +0100

    Added new projects and fixed minor css flaws

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -26,6 +26,28 @@ const projects = [
     ],
   },
   {
+    title: 'Machine Learning and Classification',
+    description: 'Implementation of data classifiers and manual training of neurons.',
+    tags: ['Python', 'NumPy'],
+    links: [
+      {
+        title: 'github',
+        link: 'https://github.com/Codingwizardirl/machine-learning-classification',
+      },
+    ],
+  },
+  {
+    title: 'Closest Pair Algorithm',
+    description: 'Implementation of Shamos\' closest pair algorithm.',
+    tags: ['Java'],
+    links: [
+      {
+        title: 'github',
+        link: 'https://github.com/Codingwizardirl/closest-pair-algorithm',
+      },
+    ],
+  },
+  {
     title: 'react-redux-barebone',
     description: 'Barebone React and Redux boilerplate equiped with goodies like ES6, Babel and a handy Webpack config.',
     tags: ['ReactJS', 'Redux', 'Webpack'],
@@ -37,13 +59,13 @@ const projects = [
     ],
   },
   {
-    title: 'Math Quiz',
-    description: 'Trivia Math Quiz app',
-    tags: ['C#'],
+    title: 'Tree Search Algorithms',
+    description: 'Implementation of WalkSat and DPLL tree search algorithms.',
+    tags: ['Haskell'],
     links: [
       {
         title: 'github',
-        link: '',
+        link: 'https://github.com/Codingwizardirl/tree-search-algorithms',
       },
     ],
   },
