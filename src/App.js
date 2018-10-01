99baaff1e4937752a924eae627fb0158cc66caee
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2018-10-01 16:38:05 +0100

    Add easter egg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import About from './components/About';
@@ -6,8 +7,16 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import './App.css';
+import EasterEgg from './components/EasterEgg/EasterEgg';
 
 const App = () => (
+  <div className="app">
+    <Route exact path="/" component={AppComponent} />
+    <Route exact path="/nautilus" component={EasterEgg} />
+  </div>
+  );
+
+const AppComponent = () => (
   <div className="App">
     <Navbar />
     <Banner title="Pavel Georgiev" />
---
+++ src/components/EasterEgg/EasterEgg.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// eslint-disable-next-line
+import styles from './styles.css';
+
+const EasterEgg = () => (
+  <div className="egg__container">
+    <div className="egg__text">Egg redeem token: SjhvCRl5P26CU6uOTrmO</div>
+    <div className="egg">
+      <div className="yolk">
+        <div className="face">
+          <div className="eyes" />
+          <div className="mouth" />
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+
+export default EasterEgg;
+
